050180b5ac762e512ea69b368d62e170b6c0681a
Aryon <brunettiaryon@gmail.com>
2023-09-04 20:29:17 -0300

    commit inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sistema Mentor</h1>
+    <div>
+        <h2>Faça Seu Login: </h2>
+        <form action="#" method="POST">
+            <input type="text" >
+            <input type="text" >       
+        </form>    
+    </div>
+    <div>
+        <h2> Faça seu Cadastro </h2>
+        <form action="#" method="POST">
+            <label for="cadastro-login">Login:</label>
+            <input type="text" name="" id="">
+
+            <label for="cadastro-email">Email</label>
+            <input type="text">
+
+            <label for="cadastro-senha">Senha</label>
+            <input type="password">
+
+            <label for="cadastro-senha-confirmacao">Confirme sua Senha</label>
+            <input type="text">
+        </form>
+    </div>
+</body>
+</html>
